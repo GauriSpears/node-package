a09534b24e6a1fdd1bae2eea02e3c032be708ce2
GauriSpears <Gauri@nextmail.ru>
2026-09-20 11:37:02 +0500

    Add files via upload

--- scripts/install-prereq.sh
+++ scripts/install-prereq.sh
@@ -79,6 +79,6 @@ elif [ "${GETPM}" == "dnf" ]; then
   dnf -y update
   GCCLATEST=$(dnf repoquery --available --qf '%{name}' 'gcc-toolset-[0-9]*' | \
     grep -E '^gcc-toolset-[0-9]+$' | sort -V | tail -1)
-  dnf -y install $"GCCLATEST" kernel-devel make git autoconf automake libtool git openssl openssl-devel cmake
+  dnf -y install "$GCCLATEST" kernel-devel make git autoconf automake libtool git openssl openssl-devel cmake
 fi
 exit 0
