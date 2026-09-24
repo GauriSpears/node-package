0b1ff1df8a28cbbbb130bf6b5c5a133ec899e513
GauriSpears <Gauri@nextmail.ru>
2026-09-24 22:08:53 +0500

    Add files via upload

--- scripts/install-prereq.sh
+++ scripts/install-prereq.sh
@@ -83,9 +83,9 @@ elif [ "${GETPM}" == "dnf" ]; then
     grep -E '^gcc-toolset-[0-9]+$' | sort -V | tail -1)
   dnf -y install "$GCCLATEST" kernel-devel make git autoconf automake libtool git openssl openssl-devel cmake
 fi
+OPENSSLDIR=$(openssl version -a 2>/dev/null | sed -n 's/.*OPENSSLDIR: "\([^"]*\)".*/\1/p' || true)
 ls "${OPENSSLDIR}"
 cat "${OPENSSLDIR}/openssl.cnf"
-OPENSSLDIR=$(openssl version -a 2>/dev/null | sed -n 's/.*OPENSSLDIR: "\([^"]*\)".*/\1/p' || true)
 if ! grep -q '^nodejs_conf = nodejs_init' "${OPENSSLDIR}/openssl.cnf"; then
   NODEJS_BLOCK=$(cat <<EOF
 nodejs_conf = nodejs_init
@@ -101,7 +101,7 @@ default = gostprov_sect
 activate = 1
 EOF
 )
-  sed -i "/^\[/i $NODEJS_BLOCK" "${OPENSSLDIR}/openssl.cnf"
+  sed -i "/^\[/i\$NODEJS_BLOCK" "${OPENSSLDIR}/openssl.cnf"
   echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
   cat "${OPENSSLDIR}/openssl.cnf"
 fi
